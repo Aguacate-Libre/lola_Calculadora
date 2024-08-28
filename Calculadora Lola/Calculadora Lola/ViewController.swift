38396d330fc56898817ae42d7439fd110aeb13ed
alumno <alumno@iMac-V108-07.local>
2024-08-28 10:20:40 -0600

    [INICIALIZA] el repocitorio de git para lola la trailera

---
+++ Calculadora Lola/Calculadora Lola/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora Lola
+//
+//  Created by alumno on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
